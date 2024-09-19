8a981c1c361a56f9008ea93a26fae4d1e00e42b1
AugustLoving <auglov536@ihmeducation.se>
2024-09-19 15:54:31 +0200

    DiceGame DONE

--- A1/js/app.js
+++ A1/js/app.js
@@ -1,5 +1,5 @@
 // Variables
-
+let result = document.getElementById("result");
 
 
 // Buttons
@@ -8,16 +8,26 @@ let button0 = document.getElementById("button0");
 
 // Texts
 let playerRollText = document.getElementById("playerRollText");
+let computerRollText = document.getElementById("computerRollText");
+let playerScoreText = document.getElementById("playerScoreText");
+let computerScoreText = document.getElementById("computerScoreText");
+let drawScoreText = document.getElementById("drawScoreText");
 
 
 // Data
 let playerRoll = 0;
+let computerRoll = 0;
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
 
 
 // Processes
 button0.addEventListener("click", function() {
  getRandomNumberOneToSixForPlayer();
-
+  getRandomNumberOneToSixForComputer();
+  showEvaluation();
+  showScores();
 });
 
 
@@ -26,10 +36,43 @@ function getRandomNumberOneToSixForPlayer(){
   playerRoll =  Math.floor(Math.random()*6) + 1;
   showPlayerRollResult();
 }
+function getRandomNumberOneToSixForComputer(){
+  computerRoll =  Math.floor(Math.random()*6) + 1;
+  showComputerRollResult();
+}
+
+function evaluate(num1, num2){
+  if (num1 > num2) {
+    playerScore++;
+    return "YOU WIN😁😁😁!"
+  }
+  else if (num1 < num2) {
+    computerScore++;
+      return "YOU LOSE😢😢😢!"
+    }
+  else{
+    drawScore++
+    return "DRAW!"
+  }
+}
+
 
 
 // Views
 function showPlayerRollResult(){
-playerRollText.innerHTML = playerRoll;
+playerRollText.innerHTML = "Player rolled: " + playerRoll;
+}
+function showComputerRollResult(){
+  computerRollText.innerHTML = "Computer rolled: " + computerRoll;
+}
+function showEvaluation(){
+  resultText = evaluate(playerRoll, computerRoll);
+  result.innerHTML = resultText;
+}
+
+function showScores(){
+  playerScoreText.innerHTML = "Player score: " + playerScore;
+  computerScoreText.innerHTML = "Computer score: " + computerScore;
+  drawScoreText.innerHTML = "Draw score: " + drawScore;
 }
 
